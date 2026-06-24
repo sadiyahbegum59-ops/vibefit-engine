79b010adb3b9860a6b304c7517d4963242fb6c0f
sadiyahbegum59-ops <sadiyahbegum59@gmail.com>
2026-06-24 14:30:53 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,34 @@
+OUTFIT_DATABASE = {
+
+ "college": {
+   "minimalist": {
+      "calm": [
+        "Beige kurti + sneakers",
+        "White shirt + jeans"
+      ],
+
+      "confident": [
+        "Black top + cargo pants",
+        "Oversized shirt + watch"
+      ]
+   }
+ },
+
+ "party": {
+   "bold": {
+      "excited": [
+        "Black coord set + heels",
+        "Satin dress + accessories"
+      ]
+   }
+ },
+
+ "eid": {
+   "elegant": {
+      "happy": [
+        "Embroidered suit + chiffon hijab",
+        "Silk suit + bracelet"
+      ]
+   }
+ }
+}
